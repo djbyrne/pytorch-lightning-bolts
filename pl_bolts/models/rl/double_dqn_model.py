0b602dc029e10ae344cc0772d82c529e339ae5c4
Donal Byrne <donaljbyrne@me.com>
2020-09-23 08:47:29 +0100

    Fixed Double DQN Loss

--- pl_bolts/models/rl/double_dqn_model.py
+++ pl_bolts/models/rl/double_dqn_model.py
@@ -113,7 +113,6 @@ if __name__ == '__main__':
     parser = pl.Trainer.add_argparse_args(parser)
 
     # model args
-    parser = cli.add_base_args(parser)
     parser = DoubleDQN.add_model_specific_args(parser)
     args = parser.parse_args()
 
